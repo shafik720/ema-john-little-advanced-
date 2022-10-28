f4c5b0bad82dd41e6a7bfa631a4f40a4deae6a2e
shafik720 <shafikrasel5@gmail.com>
2022-10-28 10:36:04 +0600

    dfgd

---
+++ src/Components/Body/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Body.css';
+
+const Body = () => {
+    return (
+        <div>
+            <h2>Hello  from body</h2>
+        </div>
+    );
+};
+
+export default Body;
