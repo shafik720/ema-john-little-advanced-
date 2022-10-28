7aa333095e12c54d206aeb7d5fcf1393783e404e
shafik720 <shafikrasel5@gmail.com>
2022-10-28 10:52:05 +0600

    dfgd

--- src/Components/Body/Body.js
+++ src/Components/Body/Body.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Body.css';
 
 const Body = () => {
+    const[product, setProduct] = useState([]);
+    useEffect(()=>{
+        fetch('products.json')
+        .then(res=>res.json())
+        .then(data=>setProduct(data))
+    },[])
     return (
         <div className="body-div">
-            <Product></Product>
-            <Cart></Cart>
+            <div className="left-div">
+                {
+                    product.map(index=><Product
+                        index = {index}
+                    ></Product>)
+                }
+            </div>
+            <div className="right-div">
+                <Cart></Cart>
+            </div>
         </div>
     );
 };
